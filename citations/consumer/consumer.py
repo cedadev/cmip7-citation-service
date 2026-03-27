19beae540d4594c8996978d18adaa4e91b5ea0db
dwest77a <daniel.westwood@stfc.ac.uk>
2026-03-27 11:46:54 +0000

    Consolidated all create/update methods

---
+++ citations/consumer/consumer.py
@@ -0,0 +1,167 @@
+__author__ = "Daniel Westwood"
+__contact__ = "daniel.westwood@stfc.ac.uk"
+__copyright__ = "Copyright 2026 United Kingdom Research and Innovation"
+
+## Consumer Unit for Kafka queues, able to utilise the ORM directly
+
+from confluent_kafka import Consumer, Producer, KafkaException
+from typing import Union
+
+from citations.utils import logstream
+import citations.models as tables
+import citations.serializers as serializers
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logstream)
+logger.propagate = False
+
+
+class KafkaConsumer:
+    def __init__(
+        self,
+        config: Union[dict, None] = None,
+        topics: Union[list, None] = None,
+        timeout: Union[int, None] = None,
+    ):
+
+        if config is not None:
+            # Front: Writes made to write_request -> send_message
+            # Back: Writes received via start(msg_received) -> process_message -> handle_message
+            self.consumer = Consumer(config)
+            self.producer = Producer(config)
+        else:
+            # Writes made directly from write_request -> handle_update
+            self.consumer = None
+            self.producer = None
+
+        self.timeout = timeout or 5000  # ms
+        self.topics = topics
+
+    def start(self):
+        """
+        Consumer Listener Loop Start Function
+
+        Runs ONLY on Listener deployment, in which case write_request CANNOT be utilised
+        as the management command is blocking this from being used.
+        """
+
+        if self.consumer is None:
+            raise KafkaException("No configuration provided")
+
+        self.consumer.subscribe(self.topics)
+        try:
+            logger.info(
+                "Kafka consumer started. Subscribed to topics: %s",
+                self.topics,
+            )
+
+            while True:
+                message = self.consumer.poll(timeout_ms=self.timeout)
+                logger.info(
+                    "Kafka consuming message: %s",
+                    message,
+                )
+
+                if message is None:
+                    continue
+
+                self.receive_message(message)
+
+                self.consumer.commit(message=message, asynchronous=False)
+
+        except KeyboardInterrupt:
+            logger.info("Kafka consumer interrupted. Exiting...")
+
+        except KafkaException as e:
+            logger.error("Kafka exception: %s", e)
+
+        finally:
+            logger.info("Closing Kafka consumer...")
+
+            self.consumer.close()
+
+    def write_request(self, table: str, method: str, content: dict):
+        """
+        Request to update the database
+
+        This can ONLY be executed by the Frontend service as the backend
+        is stuck in the `start()` method loop.
+
+        If the consumer is defined the message system will be utilised for write
+        requesting. Otherwise the write can be made directly to the database.
+        """
+        logger.info(f'Write Request: {table}:{method} - {content}')
+
+        if self.consumer is not None:
+            self.send_message(table=table, method=method, content=content)
+        else:
+            self.handle_update(table=table, method=method, content=content)
+
+    def send_message(self, table: str, method: str, content: dict):
+        """
+        Send message for write request to the events queue.
+
+        If any formal message checks are required beyond the validation of data
+        through the Views, here is where they should go.
+        """
+
+        message = {"table": table, "method": method, "content": content}
+
+        def delivery_report(err, msg):
+            if err is not None:
+                raise ValueError(err)
+            else:
+                logger.info(
+                    f"Message {msg.key()} successfully delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}"
+                )
+
+        self.producer.produce(
+            topic=self.topics[0],
+            key="CitationSvc",
+            value=message,
+            callback=delivery_report,
+        )
+        self.producer.flush()
+
+    def receive_message(self, message):
+        """
+        Interpret message.value in terms of ORM
+
+        Note: Use a separate module to write using ORM so it can
+        be used outside the Kafka consumer.
+        """
+        self.handle_update(**dict(message.value))
+
+    def handle_update(self, table: str, method: str, content: dict):
+        """
+        Handle ANY ORM Request updates here."""
+
+        model = getattr(tables, table)
+        serializer = getattr(serializers, table+'Serializer')
+        pk = model._meta.pk.name
+
+        match method:
+            case "create":
+                instance = model.objects.create(
+                    **{c:v for c,v in content.items() if c not in serializer.Meta.relations}
+                )
+                for r in serializer.Meta.relations:
+                    if r in content:
+                        getattr(instance, r).set(content[r])
+                instance.save()
+
+            case "update":
+                # Must have already validated that the primary key exists and does not change - frontend
+                instance = model.objects.get(**{pk: content[pk]})
+                content.pop(pk)
+                for attr, value in content.items():
+                    if attr in serializer.Meta.relations:
+                        attr_i = getattr(instance, attr)
+                        attr_i.set(value)
+                    else:
+                        setattr(instance, attr, value)
+                instance.save()
+
+            case "delete":
+                model.objects.get(**{pk: content[pk]}).delete()
---
+++ citations/consumer/write.py
@@ -0,0 +1,75 @@
+from django.db import models
+from rest_framework import serializers
+from citations.consumer import KafkaConsumer
+from django.conf import settings
+from typing import Union
+
+def chain_new_objects(
+        data: dict, 
+        serializer: serializers.ModelSerializer, 
+        model: type[models.Model], 
+        filter_kwargs: list, 
+        optionals: list = None
+    ) -> models.Model:
+    """
+    Validate new model instances.
+    """
+
+    optionals = optionals or []
+    filters = {k: data[k] for k in filter_kwargs}
+    for opt in optionals:
+        if data.get(opt):
+            filters[opt] = data[opt]
+    instance = model.objects.filter(**filters)
+
+    # Create instance if not specified.
+    if not instance:
+        serial = serializer(data=data)
+        serial.is_valid(raise_exception=True)
+        instance = create_instance(model, required_fields=serializer.Meta.required_fields, **dict(serial.validated_data))
+    else:
+        instance = instance[0]
+
+    # Should return newly created instance or existing one
+    return instance
+
+def create_instance(model: models.Model, required_fields: list, **kwargs):
+    """
+    Send message to consumer to create new instance"""
+
+    consumer = KafkaConsumer(
+        settings.CONSUMER_CONFIG,
+        settings.CONSUMER_TOPICS,
+    )
+    consumer.write_request(
+        table=model._meta.label.split('.')[-1],
+        method='create',
+        content=kwargs
+    )
+
+    # Use settings.QUEUE_AWAIT_TIMEOUT
+    id_kwargs = {k:v for k,v in kwargs.items() if k in required_fields}
+
+    # Get newly created model from the table. Wait until it has been created.
+    return model.objects.get(**id_kwargs)
+
+def update_instance(model: models.Model, required_fields: list, **kwargs):
+    """
+    Send message to consumer to update existing instance
+    """
+    
+    consumer = KafkaConsumer(
+        settings.CONSUMER_CONFIG,
+        settings.CONSUMER_TOPICS,
+    )
+
+    consumer.write_request(
+        table=model._meta.label.split('.')[-1],
+        method='update',
+        content=kwargs
+    )
+
+    id_kwargs = {k:v for k,v in kwargs.items() if k in required_fields}
+
+    # Await update confirmation
+    return model.objects.get(**id_kwargs)
