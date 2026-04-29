b2feef33de2bc3c1ea2bb2b7453499f72b725c08
dwest77a <daniel.westwood@stfc.ac.uk>
2026-04-29 14:51:23 +0100

    Added management commands

---
+++ citations/management/commands/new_token.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "Runs backend listener"
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str, help="Username")
+        parser.add_argument('-f','--force_create', dest='force_create', action='store_true')
+
+    def handle(self, username: str, force_create: bool = False, **kwargs):
+        token(username, force_create=force_create)
+
+def token(username: str, force_create: bool):
+
+    if not User.objects.filter(username=username):
+        u = User.objects.create(username=username)
+        u.save()
+
+    user = User.objects.get(username=username)
+
+    if force_create:
+        Token.objects.filter(user=user).delete()
+    token, created = Token.objects.get_or_create(user=user)
+
+    print(token)
