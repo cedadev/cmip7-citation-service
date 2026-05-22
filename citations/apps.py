ae4d5cf7df917ba53f334759b9e9fe4ae0859222
dwest77a <114070222+dwest77a@users.noreply.github.com>
2026-05-22 12:28:37 +0000

    Deploying to gh-pages from @ cedadev/cmip7-citation-service@255a1abd418603f198d2cca708cff6ed733897f5 🚀

--- citations/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CitationsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "citations"
-
-    def ready(self):
-        from citations import signals as signals
--- citations/external/__init__.py
+++
@@ -1,3 +0,0 @@
-from .minting import publish_record as publish_record
-from .minting import resolve_drs as resolve_drs
-from .stac import post_stac_esgf as post_stac_esgf
