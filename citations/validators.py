84f679d269fc17a8eea0dbe9d2264f2a3b43daab
dwest77a <daniel.westwood@stfc.ac.uk>
2026-05-22 12:02:08 +0100

    Streamlined validation

--- citations/validators.py
+++ citations/validators.py
@@ -17,7 +17,6 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logstream)
 logger.propagate = False
 
-
 def validate_orcid(orcid: Union[str,None]):
     """
     Validate ORCID is either none or it exists"""
@@ -56,6 +55,7 @@ def validate_country(country: Union[str,None]):
 def validate_component(
         component: str, 
         label: str, 
+        project_id: str,
         requested: Union[str,None] = None, 
         raise_exception: bool = False,
         repo: str = settings.CV_REPO):
@@ -64,9 +64,6 @@ def validate_component(
     """
 
     if ev:
-        project_id = 'CMIP7'
-        if repo != settings.CV_REPO:
-            project_id = 'CORDEX-CMIP6'
 
         logger.info(f'Validating {component} as {label} in {project_id} using ESGF Vocabs')
         term_result = ev.get_term_in_collection(term_id=component.lower(), project_id=project_id.lower(), collection_id=label.lower())
@@ -88,73 +85,3 @@ def validate_component(
         if requested:
             return r.json()[requested]
     
-def validate_cordex_facets(data: dict, raise_exceptions: bool = False):
-
-    if settings.CORDEX_CV_REPO is None:
-        raise ValueError('Misconfigured for CORDEX Facet validation')
-    
-    validate_component(data.get('mip_era').replace('CORDEX-',''), 'mip_era', 
-                       raise_exception=raise_exceptions, repo=settings.CORDEX_CV_REPO)
-    
-    validate_component(data.get('activity_id'), 'activity_id', 
-                       raise_exception=raise_exceptions, repo=settings.CORDEX_CV_REPO)
-    
-    validate_component(data.get('domain_id'), 'domain_id', 
-                       raise_exception=raise_exceptions, repo=settings.CORDEX_CV_REPO)
-    
-    validate_component(data.get('institution_id'), 'institution_id', 
-                       raise_exception=raise_exceptions, repo=settings.CORDEX_CV_REPO)
-    
-    validate_component(data.get('experiment_id'), 'driving_experiment_id', 
-                       raise_exception=raise_exceptions, repo=settings.CORDEX_CV_REPO)
-    
-    validate_component(data.get('source_id'), 'source_id', 
-                       raise_exception=raise_exceptions, repo=settings.CORDEX_CV_REPO)
-
-def validate_cmip7_facets(data: dict, raise_exceptions: bool = False):
-
-    if settings.CV_REPO is None:
-        raise ValueError('Misconfigured for CMIP7 Facet validation')
-
-    validate_component(data.get('mip_era'), 'mip_era', raise_exception=raise_exceptions)
-    validate_component(data.get('experiment_id'), 'experiment',raise_exception=raise_exceptions)
-    validate_component(data.get('institution_id'), 'institution',raise_exception=raise_exceptions)
-    validate_component(data.get('source_id'), 'source',raise_exception=raise_exceptions)
-
-    experiments = validate_component(data.get('activity_id'), 'activity', requested='experiments') or []
-    if data.get('experiment_id') not in experiments:
-        if raise_exceptions:
-            raise ValidationError(f'{data.get('experiment_id')} not valid for {data.get('activity_id')}: Valid experiments are {experiments}')
-
-def validate_title(title: str, raise_exceptions = False):
-    """
-    Validate title parameter for CMIP7 citation record
-    """
-
-    try:
-        mip_era, activity_id, institution_id, source_id, experiment_id = title.split('.')
-    except ValueError:
-        if raise_exceptions:
-            raise ValidationError('Title does not have all required components')
-        return {}
-    
-    if settings.CV_REPO is not None:
-        validate_component(mip_era, 'mip_era', raise_exception=raise_exceptions)
-        validate_component(experiment_id, 'experiment',raise_exception=raise_exceptions)
-        validate_component(institution_id, 'institution',raise_exception=raise_exceptions)
-        validate_component(source_id, 'source',raise_exception=raise_exceptions)
-
-        experiments = validate_component(activity_id, 'activity', requested='experiments') or []
-        if experiment_id not in experiments:
-            if raise_exceptions:
-                raise ValidationError(f'{experiment_id} not valid for {activity_id}: Valid experiments are {experiments}')
-            else:
-                experiment_id = ''
-        
-    return {
-        'mip_era':mip_era,
-        'activity_id':activity_id,
-        'institution_id':institution_id,
-        'source_id':source_id,
-        'experiment_id':experiment_id
-    }
