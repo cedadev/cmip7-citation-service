5dbbd833585fc030a73f3ceaeeb9816180f01c84
dwest77a <daniel.westwood@stfc.ac.uk>
2026-05-22 12:01:31 +0100

    Migrated all facet mappings to single file

--- citations/utils.py
+++ citations/utils.py
@@ -10,24 +10,3 @@ logstream = logging.StreamHandler()
 
 formatter = logging.Formatter('%(levelname)s [%(name)s]: %(message)s')
 logstream.setFormatter(formatter)
-
-LABEL_MAPPINGS = {
-    'activity':'activity_id',
-    'experiment': 'experiment_id',
-    'source': 'source_id',
-    'institution': 'institution_id',
-    'domain': 'domain_id'
-}
-
-CORE_FACETS = [
-    'mip_era',
-    'activity_id',
-    'experiment_id',
-    'source_id',
-    'institution_id',
-]
-
-ESGVOC_FACET_LABELS = {
-    'cmip7':['mip_era','activity','institution','source','experiment'],
-    'cordex-cmip6':['mip_era','activity','institution','source','driving_experiment', 'domain']
-}
